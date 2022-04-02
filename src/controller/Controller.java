eaa10da6473b914eda2d9c70ae81be028592352c
SamAenSeidhe <samuelcg132001@outlook.com>
2022-04-01 22:08:05 -0500

    v0.3.2

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -22,7 +22,7 @@ public class Controller implements ActionListener{
 	public Controller () {
 		VenPrincipal = new MainWin();
 		VenPrincipal.setVisible(true);
-		goToCatalogue();
+		goToLogIn();
 				
 		System.out.println("Cargando...");
 		
